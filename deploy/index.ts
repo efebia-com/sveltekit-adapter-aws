637edcdaeca41bca7ccf25a1986e8c917a593c8e
simone mastella <simonemastella@gmail.com>
2024-04-17 15:37:47 +0200

    update

--- deploy/index.ts
+++ deploy/index.ts
@@ -10,6 +10,6 @@ new AWSAdapterStack(app, process.env.STACKNAME!, {
   zoneName: process.env.ZONE_NAME!,
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
+    region: 'eu-west-1',
   },
 });
