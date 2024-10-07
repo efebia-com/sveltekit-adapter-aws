fc7fae6c375aa43ab95806c966f4d274fe58206a
stemasuelli999 <118314835+steeeee99@users.noreply.github.com>
2024-10-07 10:43:07 +0200

    fix hosted zone (#1)
    Co-authored-by: ste <stefano.masuelli1999@gmail.com>

--- deploy/index.ts
+++ deploy/index.ts
@@ -7,7 +7,10 @@ Tags.of(app).add('app', 'sveltekit-adapter-aws-webapp');
 
 new AWSAdapterStack(app, process.env.STACKNAME!, {
   FQDN: process.env.FQDN!,
-  zoneName: process.env.ZONE_NAME!,
+  hostedZone: {
+    zoneName: process.env.HOSTED_ZONE_NAME!,
+    hostedZoneId: process.env.HOSTED_ZONE_ID!
+  },
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
